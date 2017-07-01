9ed1a1ce4f4be3bbc9f8b9e41e8615e0828e5106
130bernardo130 <bernardo.caco666@gmail.com>
2017-07-01 11:04:42 -0300

    sql do .mwb novo

--- modelo.sql
+++ modelo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 01-Jul-2017 às 15:16
+-- Generation Time: 01-Jul-2017 às 16:04
 -- Versão do servidor: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -32,9 +32,10 @@ REPEAT
 
 INSERT INTO lojas VALUES (
     DEFAULT,
-    concat('nome', @x),
-    concat("Endereço" , @x),
-    concat("tel ", @x));
+    concat("nome ", @x),
+    concat("Endereço " , @x),
+    concat("tel ", @x)
+    );
 INSERT INTO funcionarios VALUES(
     DEFAULT,
     CONCAT("Nome ",@x),
@@ -50,15 +51,15 @@ INSERT INTO clientes VALUES(
     DEFAULT,
     CONCAT("Nome ",@x),
     CURRENT_DATE,
-    concat("Endereço" , @x),
-    concat("CPF " + @x),
+    concat("Endereço " , @x),
+	concat("CPF ",@x),
     concat("Celular " , @x),
     concat("categoria " , @x),
     concat("email" , @x,"@hotmail.com"),
     concat("origem " , @x),
     concat("funcionario " , @x),
     @x,
-    DEFAULT,
+    CURRENT_TIMESTAMP,
     concat("comentario ", @x)
     );
   
@@ -68,7 +69,7 @@ INSERT into tarefas VALUES(
     DEFAULT,
     @x,
     ceil(rand() * @val),
-    DEFAULT,
+    CURRENT_TIMESTAMP,
     concat("tipo " , @x),
     CURRENT_DATE,
     concat("comentario " , @x));
@@ -79,7 +80,7 @@ INSERT into tarefas VALUES(
      @x,
      concat("cliente " , @x),
      @x,
-     DEFAULT,
+     CURRENT_TIMESTAMP,
      concat("tipo " , @x),
      concat("comentario " , @x),
      rand() * 5000);
@@ -129,21 +130,6 @@ CREATE TABLE `clientes` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Extraindo dados da tabela `clientes`
---
-
-INSERT INTO `clientes` (`idCliente`, `Nome`, `data_de_nascimento`, `Endereço`, `CPF`, `Celular`, `Categoria`, `Email`, `Origem`, `Funcionario_que_cadastrou`, `Funcionario_idFuncionario`, `Data_de_cadastro`, `Comentario`) VALUES
-(1, 'Nome 1', '2017-07-01', 'Endereço1', '1', 'Celular 1', 'categoria 1', 'email1@hotmail.com', 'origem 1', 'funcionario 1', 1, '2017-07-01 10:10:10', 'comentario 1'),
-(2, 'Nome 2', '2017-07-01', 'Endereço2', '2', 'Celular 2', 'categoria 2', 'email2@hotmail.com', 'origem 2', 'funcionario 2', 2, '2017-07-01 10:10:10', 'comentario 2'),
-(3, 'Nome 3', '2017-07-01', 'Endereço3', '3', 'Celular 3', 'categoria 3', 'email3@hotmail.com', 'origem 3', 'funcionario 3', 3, '2017-07-01 10:10:11', 'comentario 3'),
-(4, 'Nome 4', '2017-07-01', 'Endereço4', '4', 'Celular 4', 'categoria 4', 'email4@hotmail.com', 'origem 4', 'funcionario 4', 4, '2017-07-01 10:10:11', 'comentario 4'),
-(5, 'Nome 5', '2017-07-01', 'Endereço5', '5', 'Celular 5', 'categoria 5', 'email5@hotmail.com', 'origem 5', 'funcionario 5', 5, '2017-07-01 10:10:12', 'comentario 5'),
-(6, 'Nome 6', '2017-07-01', 'Endereço6', '6', 'Celular 6', 'categoria 6', 'email6@hotmail.com', 'origem 6', 'funcionario 6', 6, '2017-07-01 10:10:12', 'comentario 6'),
-(7, 'Nome 7', '2017-07-01', 'Endereço7', '7', 'Celular 7', 'categoria 7', 'email7@hotmail.com', 'origem 7', 'funcionario 7', 7, '2017-07-01 10:10:12', 'comentario 7'),
-(8, 'Nome 8', '2017-07-01', 'Endereço8', '8', 'Celular 8', 'categoria 8', 'email8@hotmail.com', 'origem 8', 'funcionario 8', 8, '2017-07-01 10:10:12', 'comentario 8'),
-(9, 'Nome 9', '2017-07-01', 'Endereço9', '9', 'Celular 9', 'categoria 9', 'email9@hotmail.com', 'origem 9', 'funcionario 9', 9, '2017-07-01 10:10:12', 'comentario 9');
-
---
 -- Acionadores `clientes`
 --
 DELIMITER $$
@@ -185,21 +171,6 @@ CREATE TABLE `funcionarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Extraindo dados da tabela `funcionarios`
---
-
-INSERT INTO `funcionarios` (`idFuncionario`, `Nome`, `Usuario`, `Senha`, `isAdm`, `Data_de_cadastro`) VALUES
-(1, 'Nome 1', 'Usuario 1', 'Senha 1', 1, '2017-07-01 10:10:10'),
-(2, 'Nome 2', 'Usuario 2', 'Senha 2', 1, '2017-07-01 10:10:10'),
-(3, 'Nome 3', 'Usuario 3', 'Senha 3', 1, '2017-07-01 10:10:11'),
-(4, 'Nome 4', 'Usuario 4', 'Senha 4', 1, '2017-07-01 10:10:11'),
-(5, 'Nome 5', 'Usuario 5', 'Senha 5', 1, '2017-07-01 10:10:11'),
-(6, 'Nome 6', 'Usuario 6', 'Senha 6', 1, '2017-07-01 10:10:12'),
-(7, 'Nome 7', 'Usuario 7', 'Senha 7', 1, '2017-07-01 10:10:12'),
-(8, 'Nome 8', 'Usuario 8', 'Senha 8', 1, '2017-07-01 10:10:12'),
-(9, 'Nome 9', 'Usuario 9', 'Senha 9', 1, '2017-07-01 10:10:12');
-
---
 -- Acionadores `funcionarios`
 --
 DELIMITER $$
@@ -231,21 +202,6 @@ CREATE TABLE `lojas` (
   `Telefone` varchar(15) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Extraindo dados da tabela `lojas`
---
-
-INSERT INTO `lojas` (`idLoja`, `Nome`, `Endereço`, `Telefone`) VALUES
-(1, 'nome1', 'Endereço1', 'tel 1'),
-(2, 'nome2', 'Endereço2', 'tel 2'),
-(3, 'nome3', 'Endereço3', 'tel 3'),
-(4, 'nome4', 'Endereço4', 'tel 4'),
-(5, 'nome5', 'Endereço5', 'tel 5'),
-(6, 'nome6', 'Endereço6', 'tel 6'),
-(7, 'nome7', 'Endereço7', 'tel 7'),
-(8, 'nome8', 'Endereço8', 'tel 8'),
-(9, 'nome9', 'Endereço9', 'tel 9');
-
 -- --------------------------------------------------------
 
 --
@@ -256,21 +212,6 @@ CREATE TABLE `lojas_has_funcionarios` (
   `Lojas_idLoja` int(10) UNSIGNED NOT NULL,
   `Funcionarios_idFuncionario` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `lojas_has_funcionarios`
---
-
-INSERT INTO `lojas_has_funcionarios` (`Lojas_idLoja`, `Funcionarios_idFuncionario`) VALUES
-(1, 1),
-(2, 2),
-(3, 3),
-(4, 4),
-(5, 5),
-(6, 6),
-(7, 7),
-(8, 8),
-(9, 9);
 
 --
 -- Acionadores `lojas_has_funcionarios`
@@ -299,21 +240,6 @@ CREATE TABLE `negocios` (
   `Comentario` varchar(250) DEFAULT NULL,
   `Valor_do_negocio` decimal(19,4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `negocios`
---
-
-INSERT INTO `negocios` (`idNegocio`, `Funcionario`, `idFuncionario`, `Cliente`, `idCliente`, `Data_do_Negocio`, `Tipo_de_negocio`, `Comentario`, `Valor_do_negocio`) VALUES
-(1, 'funcionario 1', 1, 'cliente 1', 1, '2017-07-01 10:10:10', 'tipo 1', 'comentario 1', '1820.7492'),
-(2, 'funcionario 2', 2, 'cliente 2', 2, '2017-07-01 10:10:10', 'tipo 2', 'comentario 2', '1760.1506'),
-(3, 'funcionario 3', 3, 'cliente 3', 3, '2017-07-01 10:10:11', 'tipo 3', 'comentario 3', '1251.0238'),
-(4, 'funcionario 4', 4, 'cliente 4', 4, '2017-07-01 10:10:11', 'tipo 4', 'comentario 4', '1529.0096'),
-(5, 'funcionario 5', 5, 'cliente 5', 5, '2017-07-01 10:10:12', 'tipo 5', 'comentario 5', '3117.4032'),
-(6, 'funcionario 6', 6, 'cliente 6', 6, '2017-07-01 10:10:12', 'tipo 6', 'comentario 6', '419.5521'),
-(7, 'funcionario 7', 7, 'cliente 7', 7, '2017-07-01 10:10:12', 'tipo 7', 'comentario 7', '1396.7685'),
-(8, 'funcionario 8', 8, 'cliente 8', 8, '2017-07-01 10:10:12', 'tipo 8', 'comentario 8', '107.9197'),
-(9, 'funcionario 9', 9, 'cliente 9', 9, '2017-07-01 10:10:12', 'tipo 9', 'comentario 9', '3878.2435');
 
 --
 -- Acionadores `negocios`
@@ -353,21 +279,6 @@ CREATE TABLE `tarefas` (
   `Data_Limite` date NOT NULL,
   `Comentario` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `tarefas`
---
-
-INSERT INTO `tarefas` (`idTarefas`, `idFuncionario_Criador`, `idFuncionario_Delegado`, `Data_Criaçao`, `Tipo_de_tarefa`, `Data_Limite`, `Comentario`) VALUES
-(1, 1, 1, '2017-07-01 10:10:10', 'tipo 1', '2017-07-01', 'comentario 1'),
-(2, 2, 1, '2017-07-01 10:10:10', 'tipo 2', '2017-07-01', 'comentario 2'),
-(3, 3, 3, '2017-07-01 10:10:11', 'tipo 3', '2017-07-01', 'comentario 3'),
-(4, 4, 1, '2017-07-01 10:10:11', 'tipo 4', '2017-07-01', 'comentario 4'),
-(5, 5, 3, '2017-07-01 10:10:12', 'tipo 5', '2017-07-01', 'comentario 5'),
-(6, 6, 1, '2017-07-01 10:10:12', 'tipo 6', '2017-07-01', 'comentario 6'),
-(7, 7, 1, '2017-07-01 10:10:12', 'tipo 7', '2017-07-01', 'comentario 7'),
-(8, 8, 7, '2017-07-01 10:10:12', 'tipo 8', '2017-07-01', 'comentario 8'),
-(9, 9, 1, '2017-07-01 10:10:12', 'tipo 9', '2017-07-01', 'comentario 9');
 
 --
 -- Acionadores `tarefas`
@@ -459,27 +370,27 @@ ALTER TABLE `alteraçoes`
 -- AUTO_INCREMENT for table `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `idCliente` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `idCliente` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `funcionarios`
 --
 ALTER TABLE `funcionarios`
-  MODIFY `idFuncionario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `idFuncionario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `lojas`
 --
 ALTER TABLE `lojas`
-  MODIFY `idLoja` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `idLoja` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `negocios`
 --
 ALTER TABLE `negocios`
-  MODIFY `idNegocio` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `idNegocio` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `tarefas`
 --
 ALTER TABLE `tarefas`
-  MODIFY `idTarefas` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `idTarefas` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
